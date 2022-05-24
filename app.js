1d631a25600529b95a2bee9fe3a2ae64a2b7d53f
mohitlekh <mohitlekh@gmail.com>
2022-05-24 13:01:54 +0530

    commitinitial

---
+++ app.js
@@ -0,0 +1,75 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
+
+var token =""
+var res;
+ async function a(data,token){  
+    var config = {
+      method: 'post',
+      url: 'https://demo.credy.in/api/v1/usermodule/login/',
+      headers: { 
+        "Content-Type": "application/json",
+      },
+      data : data
+    };
+    token =  await axios(config)
+    .then( (response)=> {
+        return JSON.stringify(response.data.data.token)})
+    .catch(function (error) {
+    console.log(error);
+    });
+    window.localStorage.setItem("token", token);
+    if(window.localStorage.getItem("token") == token ){
+      movies(token);
+    }
+    else {
+      document.getElementById('signInMsg').innerHTML = "Bad Credentials"
+    }
+}
+
+const button = document.querySelector('.btn-primary');
+button.addEventListener('click',()=>{
+    let userId = document.getElementById('exampleInputEmail1').value;
+    let password = document.getElementById('exampleInputPassword1').value;
+    var data = JSON.stringify({
+        "username": userId,
+        "password": password
+      });
+   console.log(data);    
+    a(data)
+   
+
+});
+
+async function movies(token){
+  console.log(token)
+  let options = {
+    method: 'GET',
+    headers: {
+        'Content-Type':'application/json',
+        'Authorization': `Token ${token}`
+    }
+  }
+  res = await fetch("https://demo.credy.in/api/v1/maya/movies/",options)
+        .then(data => {return data.json()})
+        .then(d => console.log("successcall"))
+        .catch(err => console.log(err));
+  console.log(res)
+  window.location = 'movie.html';
+  if(res != undefined || null){
+    console.log("if call")
+    Displaycards();
+  }
+  else{
+    console.log("Message")
+    let refreshMessage = document.querySelector('.moviecards').innerHTML = "refresh page to load contents";
+  }
+}
---
+++ movies.js
@@ -0,0 +1,32 @@
+$.get('index.html', null, function(text){
+    ($(text).find('#name'));
+});
+async function movies(token){
+    console.log(token)
+    let options = {
+      method: 'GET',
+      headers: {
+          'Content-Type':'application/json',
+          'Authorization': `Token ${token}`
+      }
+    }
+    res = await fetch("https://demo.credy.in/api/v1/maya/movies/",options)
+          .then(data => {return data.json()})
+          .then(d => console.log("successcall"))
+          .catch(err => console.log(err));
+    console.log(res)
+    //window.open("movie.html")
+    if(res != undefined || null){
+      console.log("if call");
+    }
+    else {
+        console.log("Message")
+        let refreshMessage = document.querySelector('.moviecards');
+        let Newp = document.createElement("p");
+        Newp.setAttribute("id","msgToRef");
+        refreshMessage.append(Newp);
+        document.getElementById('msgToRef').innerHTML = "Refresh To Load Content";
+    }
+}
+let access = window.localStorage.getItem("token");
+movies(access);
